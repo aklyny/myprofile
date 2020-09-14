950c7abfaf87c5a5df45269a4569e8ac8959d166
Aklyny <58581828+Aklyny@users.noreply.github.com>
2020-09-14 21:16:44 +0530

    profile change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,14 +17,9 @@ class Footer extends Component {
            <ul className="social-links">
               {networks}
            </ul>
-
-           <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
-           </ul>
-
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020 Aklyny</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Sri Bharathi Dass</a></li>
            </ul>
 
         </div>
